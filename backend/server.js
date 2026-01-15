f83edbfa1f38225bcf573b773a07970cce4be446
Harsh Pandey <harsh28.knp@gmail.com>
2026-01-15 09:50:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,9 @@ app.use("/api/ai/generate-explanation", protect, generateConceptExplanation)
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}))
 
 // Start Server
-// const PORT = process.env.PORT || 5000
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+}
 
 module.exports = app
